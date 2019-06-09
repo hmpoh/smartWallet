a20a4072b33e474bd7e679ebfa5e2b20bb6fd3f7
hmpoh <poh.hui.min77@gmail.com>
2019-06-09 11:44:15 +0800

    Generating QR code

--- app/src/main/java/sg/edu/tp/smartwallet/QR.java
+++ app/src/main/java/sg/edu/tp/smartwallet/QR.java
@@ -1,14 +1,67 @@
 package sg.edu.tp.smartwallet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class QR extends AppCompatActivity {
+    public final static int QRcodeWidth = 500 ;
+    private ImageView qrCode;
+    Bitmap bitmap ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+        qrCode = (ImageView) findViewById(R.id.qrCode);
 
+        try {
+            bitmap = TextToImageEncode("hello");
+            qrCode.setImageBitmap(bitmap);
+//            String path = saveImage(bitmap);  //give read write permission
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+//https://demonuts.com/generate-qr-code/
+    private Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.black):getResources().getColor(R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
     }
 }
