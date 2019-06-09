03d158d4acabc2aada4e1bb522820788aa582570
hmpoh <poh.hui.min77@gmail.com>
2019-06-09 10:50:23 +0800

    implemented login screen

--- app/src/main/java/sg/edu/tp/smartwallet/LogIn.java
+++ app/src/main/java/sg/edu/tp/smartwallet/LogIn.java
@@ -1,44 +1,77 @@
 package sg.edu.tp.smartwallet;
 
-import android.support.v4.app.Fragment;
 import android.content.Intent;
-import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.Printer;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn extends AppCompatActivity {
 
     EditText accessCode;
     EditText pin;
-    private  FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
+    private static final String TAG ="LoginScreen";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        handleUserLogin(currentUser);
+    }
+
+    private void handleUserLogin(FirebaseUser currentUser) {
+        Intent intent = new Intent(this, Transfer.class);
+        startActivity(intent);
+
+        Log.d(TAG, "handling user log in");
+    }
+
     public void onClickButtonLogin(View view){
-//        Intent intent = new Intent(this, Transfer.class);
-//        startActivity(intent);
         accessCode = (EditText) findViewById(R.id.accessCode);
         pin = (EditText) findViewById(R.id.pin);
+
+        String accessCodeString = accessCode.getText().toString() + "@smartwallet.sg";
+        String pinString = pin.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(accessCodeString, pinString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            handleUserLogin(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
