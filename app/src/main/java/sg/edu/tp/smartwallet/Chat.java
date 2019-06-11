da06f41ab4011ecddecbfe515dc56483a49b94a8
hmpoh <poh.hui.min77@gmail.com>
2019-06-11 19:53:43 +0800

    Android Group chat Application using Firebase 18 Create Group Chat Activity

--- app/src/main/java/sg/edu/tp/smartwallet/Chat.java
+++ app/src/main/java/sg/edu/tp/smartwallet/Chat.java
@@ -1,13 +1,40 @@
 package sg.edu.tp.smartwallet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton SendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        InitializFields();
     }
+
+    private void InitializFields() {
+        mToolbar = (Toolbar)findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Group Name");
+
+        SendMessageButton = (ImageButton)findViewById(R.id.send_message_button);
+        userMessageInput = (EditText) findViewById(R.id.input_group_message);
+        displayTextMessages = (TextView)findViewById(R.id.group_chat_bar_layout);
+        mScrollView = (ScrollView)findViewById(R.id.my_scroll_view);
+
+    }
+
+
 }
