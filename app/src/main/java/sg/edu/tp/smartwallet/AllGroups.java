256000eed4b1ea49f1207f0a9e692f9111a80782
hmpoh <poh.hui.min77@gmail.com>
2019-06-11 17:02:39 +0800

    Group Chat App Android Studio - Android Group chat Application using Firebase: Create Groups and store them into Firebase Database

--- app/src/main/java/sg/edu/tp/smartwallet/AllGroups.java
+++ app/src/main/java/sg/edu/tp/smartwallet/AllGroups.java
@@ -1,10 +1,22 @@
 package sg.edu.tp.smartwallet;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AllGroups extends AppCompatActivity {
 
@@ -12,6 +24,9 @@ public class AllGroups extends AppCompatActivity {
     private ViewPager myViewPager;
     private TabLayout myTabLayout;
     private TabsAccessorAdapter myTabsAccessorAdapter;
+    private DatabaseReference RootRef;
+
+//https://www.youtube.com/watch?v=sgMO1AbUJmA&list=PLxefhmF0pcPmtdoud8f64EpgapkclCllj&index=16
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +37,68 @@ public class AllGroups extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Group Chat");
 
-        myViewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
         myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
         myViewPager.setAdapter(myTabsAccessorAdapter);
 
-        myTabLayout = (TabLayout)findViewById(R.id.main_tabs);
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
         myTabLayout.setupWithViewPager(myViewPager);
+
+        RootRef = FirebaseDatabase.getInstance().getReference();
+    }
+
+
+    private void RequestNewGroup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AllGroups.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name: ");
+
+        final EditText groupNameField = new EditText(AllGroups.this);
+        groupNameField.setHint("e.g Graduation Trip");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String groupName = groupNameField.getText().toString();
+
+                if (TextUtils.isEmpty(groupName)) {
+                    Toast.makeText(AllGroups.this, "Please Write Group Name", Toast.LENGTH_SHORT).show();
+                } else {
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.cancel();
+
+            }
+        });
+
+        builder.show();
+    }
+
+    //Here is where we store the group name into the Firebase Database
+    private void CreateNewGroup(final String groupName) {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(AllGroups.this, groupName + " group is Created Successfully...", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    public void onClickButtonNewGroup(View view) {
+        RequestNewGroup();
+
     }
 }
+
